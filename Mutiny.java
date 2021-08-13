38ec335ee92f69dd2f1db855e72cbe1c1ddece8f
Sergio Rodríguez Calvo <sergiorodriguezcalvo@gmail.com>
2021-08-13 19:05:44 +0200

    initial commit

---
+++ Mutiny.java
@@ -0,0 +1,340 @@
+///usr/bin/env jbang "$0" "$@" ; exit $?
+//DEPS io.smallrye.reactive:mutiny:1.0.0
+
+import io.smallrye.mutiny.Uni;
+import io.smallrye.mutiny.Multi;
+import io.smallrye.mutiny.subscription.Cancellable;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import java.util.stream.IntStream;
+
+public class Mutiny {
+
+    public static void main(String... args) {
+
+        System.out.println();
+        System.out.println();
+        System.out.println("Exercise 1: hello Mutiny!");
+
+        //Exercise 1: hello Mutiny!
+        helloMutiny();
+
+        System.out.println();
+        System.out.println("Exercise 2: Mutiny uses a builder API");
+
+        //Exercise 2: Mutiny uses a builder API
+        builderAPI();
+
+        System.out.println();
+        System.out.println("Excercise 3: The Uni type");
+
+        //Excercise 3: The Uni type
+        uniType();
+
+        System.out.println();
+        System.out.println("Excercise 4: Subscribing to an Uni");
+
+        //Excercise 4: Subscribing to an Uni
+        subscribingUni();
+
+        System.out.println();
+        System.out.println("Excercise 5: Supplying an Uni");
+
+        //Excercise 5: Supplying an Uni
+        supplyingUni();
+
+        System.out.println();
+        System.out.println("Excercise 6: Failing Unis");
+
+        //Excercise 6: Failing Unis
+        failingUnis();
+
+        System.out.println();
+        System.out.println("Excercise 7: Operation to not produce a result");
+
+        //Excercise 7: Operation to not produce a result
+        voidUni();
+
+        System.out.println();
+        System.out.println("Excercise 8: The Multy type");
+
+        //Excercise 8: The Multy type
+        multiType();
+
+        System.out.println();
+        System.out.println("Excercise 9: Supplying a Multi");
+
+        //Excercise 9: Supplying a Multi
+        supplyingMulti();
+
+        System.out.println();
+        System.out.println("Excercise 10: Empty Multis");
+
+        //Excercise 10: Empty Multis
+        emptyMulti();
+
+        System.out.println();
+        System.out.println("Excercise 11: Managing null");
+
+        //Excercise 11: Managing null
+        uniNull();
+
+        System.out.println();
+        System.out.println("Excercise 12: From multi to uni of list");
+
+        //Excercise 12: From multi to uni of list
+        multiToUniOfList();
+
+        System.out.println();
+        System.out.println("Excercise 13: Recovering from fail");
+
+        //Excercise 13: Recovering from fail
+        recoveringFromFail();
+
+        System.out.println();
+        System.out.println("Excercise 14: Chain an uni with another one");
+
+        //Excercise 14: Chain an uni with another one
+        chainUnis();
+
+        System.out.println();
+        System.out.println("Excercise 15: Combine several unis (paralelly)");
+
+        //Excercise 15: Combine several unis (paralelly)
+        combineUnis();
+
+        System.out.println();
+        System.out.println("Excercise 16: Combine several unis (paralelly) with a failure");
+
+        //Excercise 16: Combine several unis (paralelly) with a failure
+        combineUnisWithFailure();
+
+    }
+
+    private static void helloMutiny() {
+        Uni.createFrom().item("hello")
+            .onItem().transform(item -> item + " mutiny")
+            .onItem().transform(String::toUpperCase)
+            .subscribe().with(
+                item -> System.out.println(">> " + item)
+            );
+    }
+
+    private static void builderAPI() {
+        Uni<String> uni1 = Uni.createFrom().item("hello");
+        Uni<String> uni2 = uni1.onItem().transform(item -> item + " mutiny");
+        Uni<String> uni3 = uni2.onItem().transform(String::toUpperCase);
+
+        uni3.subscribe().with(item -> System.out.println(">> " + item));
+
+        //Note that the previous program is not equivalent to the code below
+        Uni<String> uni = Uni.createFrom().item("hello");
+
+        uni.onItem().transform(item -> item + " mutiny");
+        uni.onItem().transform(String::toUpperCase);
+
+        uni.subscribe().with(item -> System.out.println(">> " + item));
+    }
+
+    private static void uniType() {
+        //A Uni<T> is a specialized stream that emits only an item or a failure.
+        Uni.createFrom().item(1)
+            .onItem().transform(i -> "hello-" + i)
+            .subscribe().with(System.out::println);
+    }
+
+    private static void subscribingUni() {
+        // Remember: if you don’t subscribe, nothing is going to happen. 
+        // What’s more, the pipeline is materialized for each subscription.
+        Cancellable cancellable = Uni.createFrom().item("subscribed")
+                                    .subscribe().with(
+                                        item -> System.out.println(item),
+                                        failure -> System.out.println("Failed with " + failure)
+                                    );
+        // Note the returned Cancellable: this object allows canceling the operation if need be.
+    }
+
+    private static void supplyingUni() {
+        //The Supplier is called for every subscriber. So, each of them will get a different value.
+        AtomicInteger counter = new AtomicInteger();
+        Uni<Integer> uni = Uni.createFrom().item(() -> counter.getAndIncrement());
+        
+        uni.subscribe().with(item -> System.out.println("Subscriber " + item));
+        uni.subscribe().with(item -> System.out.println("Subscriber " + item));
+    }
+
+    private static void failingUnis() {
+        // Pass an exception directly:
+        Uni<Integer> failed1 = Uni.createFrom().failure(new Exception("directly boom"));
+
+        // Pass a supplier called for every subscriber:
+        Uni<Integer> failed2 = Uni.createFrom().failure(() -> new Exception("supplier boom"));
+
+        failed1.subscribe().with(
+            item -> System.out.println(item),
+            failure -> System.out.println(failure.getMessage())
+        );
+
+        failed2.subscribe().with(
+            item -> System.out.println(item),
+            failure -> System.out.println(failure.getMessage())
+        );
+
+    }
+
+    private static void voidUni() {
+        Uni<Void> uni = Uni.createFrom().nullItem();
+    }
+
+    private static void multiType() {
+        Multi.createFrom().items(1, 2, 3, 4, 5)
+            .onItem().transform(i -> i * 2)
+            .select().first(3)
+            .onFailure().recoverWithItem(0)
+            .subscribe().with(System.out::println);
+    }
+
+    private static void supplyingMulti() {
+        AtomicInteger counter = new AtomicInteger();
+        Multi<Integer> multi = Multi.createFrom().items(() -> IntStream.range(counter.getAndIncrement(), counter.get() * 2).boxed());
+        multi.subscribe().with(item -> System.out.println(item));
+        multi.subscribe().with(item -> System.out.println(item));
+    }
+
+    private static void emptyMulti() {
+        Multi<String> multi = Multi.createFrom().empty();
+    }
+
+    private static Uni<String> findNameById(int id) {
+        if (id == 1)
+            return Uni.createFrom().nullItem();
+        else if (id > 1)
+            return Uni.createFrom().failure(new RuntimeException("Name does not exist"));
+        return Uni.createFrom().item("name");
+    }
+
+    private static void uniNull() {
+        //Same code, but different behavior
+
+        // Get string
+        findNameById(0)
+            .onItem().ifNull().continueWith("Name is null")
+            .onItem().ifNotNull().transform(String::toUpperCase)
+            .subscribe().with(
+                item -> System.out.println(item),
+                failure -> System.out.println(failure.getMessage())
+        );
+
+        //Get null
+        findNameById(1)
+                .onItem().ifNull().continueWith("Name is null")
+                .onItem().ifNotNull().transform(String::toUpperCase)
+                .subscribe().with(
+                item -> System.out.println(item),
+                failure -> System.out.println(failure.getMessage())
+        );
+
+        // Get failure
+        findNameById(2)
+                .onItem().ifNull().continueWith("Name is null")
+                .onItem().ifNotNull().transform(String::toUpperCase)
+                .subscribe().with(
+                item -> System.out.println(item),
+                failure -> System.out.println(failure.getMessage())
+        );
+    }
+
+    private static void multiToUniOfList() {
+        Multi<Integer> items = Multi.createFrom().items(1, 2, 3);
+
+        items.collect().asList()
+                .subscribe().with(
+                        item -> {
+                            for(Integer i: item)
+                                System.out.println(">> " + i);
+                        }
+                );
+    }
+
+    private static void recoveringFromFail() {
+        Uni.createFrom().failure(new RuntimeException("Some error"))
+                .onFailure().recoverWithItem("hello")
+                .subscribe().with(System.out::println);
+    }
+
+    private static Uni<String> invokeRemoteGreetingService(String name) {
+        return Uni.createFrom().item("hello " + name);
+    }
+
+    private static void chainUnis() {
+        Uni<String> uni = Uni.createFrom().item("serrodcal");
+
+        uni
+            .onItem().transformToUni(name -> invokeRemoteGreetingService(name)) //Calling a remote service asynchronously
+            .subscribe().with(System.out::println);
+
+        uni
+            .chain(name -> invokeRemoteGreetingService(name))
+            .subscribe().with(System.out::println);
+    }
+
+    private static Uni<Integer> doSomething(Integer i) {
+        return Uni.createFrom().item(i);
+    }
+
+    private static void combineUnis() {
+        List<Uni<?>> unis = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++){
+            Uni<Integer> uni = doSomething(i);
+            unis.add(uni);
+        }
+
+        Uni<Integer> uni = Uni.combine().all().unis(unis)
+            .combinedWith(results -> {
+                int acc = 0;
+                for (Object result: results)
+                    if(result instanceof Integer)
+                        acc += (Integer) result;
+                return acc;
+            }).onFailure().recoverWithUni(failure -> {
+                return Uni.createFrom().item(-1);
+            });
+
+        uni.subscribe().with(System.out::println);
+
+    }
+
+    private static Uni<Integer> doSomethingWrong(Integer i) {
+        if (i == 2)
+            return Uni.createFrom().failure(new RuntimeException("error"));
+        return Uni.createFrom().item(i);
+    }
+
+    private static void combineUnisWithFailure() {
+        List<Uni<?>> unis = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++){
+            Uni<Integer> uni = doSomethingWrong(i);
+            unis.add(uni);
+        }
+
+        Uni<Integer> uni = Uni.combine().all().unis(unis)
+                .combinedWith(results -> {
+                    int acc = 0;
+                    for (Object result: results)
+                        if(result instanceof Integer)
+                            acc += (Integer) result;
+                    return acc;
+                }).onFailure().recoverWithUni(failure -> {
+                    return Uni.createFrom().item(-1);
+                });
+
+        uni.subscribe().with(System.out::println);
+
+    }
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,81 @@
-# mutiny-examples
+## jbang project for Mutiny by examples
+
+This is a "virtual" project directory for the jbang script Mutiny.
+
+This lets you get content assist, debugging, etc. for Java in most modern editors and IDE's.
+
+JBang tested this to work in the following IDE's:
+
+* Eclipse
+* IntelliJ
+* Visual Studio Code
+* Netbeans
+* vi
+* emacs
+
+## Enjoy
+
+Have fun and if you like this consider following [@jbangdev](http://twitter.com/jbangdev) and give a
+star to [jbangdev/jbang](https://github.com/jbangdev/jbang).
+
+If you have issues or suggestions please [open an issue](https://github.com/jbangdev/jbang/issues/new).
+
+With love, <br/>
+[jbang.dev](https://jbang.dev)
+
+## Steps followed to generated and to use this scripting project
+
+Project created with:
+
+> jbang init Mutiny.java
+
+Project edited with:
+
+> jbang edit --open=code --live Mutiny.java
+
+Execute this project with:
+
+> jbang Mutiny.java
+
+## About Mutiny
+
+Mutiny is an Intuitive Event-Driven Reactive Programming Library for Java. It provides a simple but powerful asynchronous development model that lets you build reactive applications.
+
+Mutiny is:
+
+* Event-Driven: Mutiny places events at the core of its design. With Mutiny, you observe events, react to them, and create elegant and readable processing pipelines. A PhD in functional programming is not required.
+* Navigable: Even with smart code completion, classes with hundred of methods are confusing. Mutiny provides a navigable and explicit API driving you towards the operator you need.
+* Non-Blocking I/O: Mutiny is the perfect companion to tame the asynchronous nature of applications with non-blocking I/O. Declaratively compose operations, transform data, enforce progress, recover from failures and more.
+
+Because of Mutiny is a reactive programming library, we have to provide a definition about what's mean reactive programming:
+
+> Reactive programming is about programming with data streams.
+
+That’s it. Reactive programming is about streams and especially, observing them. It pushes that idea to its limit: with reactive programming, everything is a data stream.
+
+With reactive programming, you observe streams and implement side-effects when something flows in the stream.
+
+It’s asynchronous by nature as you don’t know when the data is going to be seen. Yet, reactive programming goes beyond this. It provides a toolbox to compose streams and process events.
+
+But, What's Reactive Stream?
+
+Well, it's a standard for asynchronous stream processing with a non-blocking back pressure.
+
+This standard provides an API based on four concepts:
+
+* Subscription: A simple interface acting as a kind of channel description and providing two methods:
+    * `cancel()`: Used by a subscriber to cancel its subscription
+    * `request(long)`: Used by a subscriber to request n messages to a Publisher
+* Publisher: A generic interface providing a single `subscribe(Subscriber)` method. At first glance considering the original publish/subscribe pattern, it might be strange for a publisher to subscribe to a subscriber. But this is actually the strength of Reactive Streams, the capability for a publisher to adapt its flow of data according to a subscriber.
+* Subscriber: A generic interface providing 4 methods:
+    * `onComplete()`: Notify the subscriber when a publisher is closed
+    * `onError(Throwable)`: Notify the subscriber when a publisher is closed with an error state
+    * `onNext(T)`: Notify the subscriber a message was sent. If everything goes right, after having processed a message, the subscriber will usually invoke then `Subscription.request(long)`.
+    * `onSubscribe(Subscription)`: Notify the subscriber that a publisher just started a subscription (through the Publisher.subscribe(Subscriber) method). This is also a way for the subscriber to keep a reference on the Subscription object provided.
+* Processor: A simple interface extending Publisher and Subscriber interfaces. In other words a Processor can acts as both a Publisher and a Subscriber.
+
+## References
+
+* [Smallrye Mutiny](https://smallrye.io/)
+* [Reactive Streams](https://www.reactive-streams.org/)
+* [An Introduction to Reactive Streams](https://teivah.medium.com/an-introduction-to-reactive-streams-fad58e21c795)
